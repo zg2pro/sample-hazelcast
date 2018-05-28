757aa67f88dd36af2f3a0d6e25165a07edf27e7a
Gregory Anne <englishblazere@free.fr>
2018-05-28 11:27:47 +0200

    Create MicroserviceApplication.java

---
+++ src/main/java/com/github/zg2pro/samples/MicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.github.zg2pro.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceApplication.class, args);
+    }
+}
