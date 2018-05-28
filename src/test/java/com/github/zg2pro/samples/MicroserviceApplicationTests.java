16b95285c5902832b3db56867ea868dda782baf6
Gregory Anne <englishblazere@free.fr>
2018-05-28 11:30:11 +0200

    Create MicroserviceApplicationTests.java

---
+++ src/test/java/com/github/zg2pro/samples/MicroserviceApplicationTests.java
@@ -0,0 +1,20 @@
+package com.github.zg2pro.samples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * This application actually needs a cloud config server to run, referenced in the bootstrap.yml
+ * @author zg2pro
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
